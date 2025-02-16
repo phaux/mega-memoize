239ea8380da3ac3a1116328136950d9a20d52f6d
Nikita <lisqxd@proton.me>
2025-02-16 17:38:19 +0100

    feat: expire option for deno KV cache

--- denoKvCache.ts
+++ denoKvCache.ts
@@ -1,6 +1,21 @@
 import type { MemoizeAsyncCache } from "./memoizeAsync.ts";
 
 /**
+ * Options for {@link denoKvCache}.
+ */
+export interface DenoKvCacheOptions<A extends readonly unknown[], R> {
+  /**
+   * Function to determine the expiration time of a cached result.
+   * If a number is provided, it will be used as the expiration time in milliseconds.
+   * If undefined, the result will not expire.
+   */
+  expireIn?:
+    | ((this: void, result: R, ...args: A) => number | undefined)
+    | number
+    | undefined;
+}
+
+/**
  * Creates a {@link MemoizeAsyncCache} using a {@link Deno.Kv} store.
  *
  * Both arguments and results are stored as is.
@@ -16,6 +31,7 @@ export function denoKvCache<
 >(
   db: Deno.Kv,
   keyPrefix: Deno.KvKey,
+  options?: DenoKvCacheOptions<A, R>,
 ): MemoizeAsyncCache<A, R> {
   return {
     async get(key) {
@@ -24,7 +40,14 @@ export function denoKvCache<
       return entry.value;
     },
     async set(key, value) {
-      await db.set([...keyPrefix, ...key], value);
+      const expireIn = typeof options?.expireIn === "function"
+        ? options.expireIn(value, ...key)
+        : options?.expireIn ?? undefined;
+      await db.set(
+        [...keyPrefix, ...key],
+        value,
+        expireIn != null ? { expireIn } : undefined,
+      );
     },
     async delete(key) {
       await db.delete([...keyPrefix, ...key]);
